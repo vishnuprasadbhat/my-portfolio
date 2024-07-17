651099a263f38ae0998c3b99d1b482abcda9f392
vishnuprasadbhat <bhatvishnu.1@gmail.com>
2024-07-17 16:44:14 +0530

    feat: added new site link

--- js/script.js
+++ js/script.js
@@ -25,6 +25,8 @@ nav.addEventListener("click", function (e) {
   } else if (id === `resume`) {
     window.location.href = `resume/Vishnu's Resume.pdf`;
     window.location.target = "_blank";
+  } else if (id === "newSite") {
+    window.open("https://vishnuprasadbhat-portfolio.vercel.app/", "_blank");
   } else if (e.target.classList.contains("main-nav-link")) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
 
@@ -54,3 +56,29 @@ const sectionAboutObserver = new IntersectionObserver(stickyNav, {
 });
 
 sectionAboutObserver.observe(sectionAbout);
+
+// Get the modal
+const modal = document.getElementById("myModal");
+// Get the button that opens the modal
+const btn = document.getElementById("myBtn");
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+const showModal = () => {
+  modal.style.display = "block";
+};
+
+// Open modal when the page loads
+window.onload = showModal;
